e4f8f671664a72250e42f4ce70ecea0349881a30
sairish2001 <sairish2001@gmail.com>
2021-02-12 20:10:19 +0530

    whitenoise added

--- PlaywithText/settings.py
+++ PlaywithText/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'PlaywithText.urls'
@@ -121,3 +122,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static'),'templates']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
